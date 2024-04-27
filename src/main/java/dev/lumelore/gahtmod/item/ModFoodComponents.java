6365824bd0f4f63b51831ac2e8dfde717cdf6686
Lumelore <itslumelore@gmail.com>
2024-04-27 01:08:17 -0500

    Added effects to some items

---
+++ src/main/java/dev/lumelore/gahtmod/item/ModFoodComponents.java
@@ -0,0 +1,35 @@
+package dev.lumelore.gahtmod.item;
+
+import dev.lumelore.gahtmod.effect.ModEffects;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.FoodComponent;
+
+public class ModFoodComponents {
+
+    // 28800 ticks is 24 minutes
+    //  9600 ticks is  8 minutes
+    //  3600 ticks is  3 minutes
+    //  2400 ticks is  2 minutes
+    //  1200 ticks is  1  minute
+
+    // Girl Power I for 8 minutes and Regeneration I for 1 minute
+    public static final FoodComponent ESTROGEN_ITEM = new FoodComponent.Builder().alwaysEdible()
+            .statusEffect(new StatusEffectInstance(ModEffects.GIRL_POWER, 9600, 0), 1f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 1200, 0), 1f).build();
+
+    // Girl Power I for 24 minutes and Regeneration II for 2 minutes
+    public static final FoodComponent STRONG_ESTROGEN_ITEM = new FoodComponent.Builder().alwaysEdible()
+            .statusEffect(new StatusEffectInstance(ModEffects.GIRL_POWER, 28800, 0), 1f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 2400, 1), 1f).build();
+
+    // Boy Power I for 8 minutes and Strength I for 1 minute
+    public static final FoodComponent TESTOSTERONE_ITEM = new FoodComponent.Builder().alwaysEdible()
+            .statusEffect(new StatusEffectInstance(ModEffects.BOY_POWER, 9600, 0), 1f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 1200, 0), 1f).build();
+
+    // Boy Power I for 24 minutes and Strength II for 2 minutes
+    public static final FoodComponent STRONG_TESTOSTERONE_ITEM = new FoodComponent.Builder().alwaysEdible()
+            .statusEffect(new StatusEffectInstance(ModEffects.BOY_POWER, 28800, 0), 1f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 2400, 1), 1f).build();
+}
--- src/main/java/dev/lumelore/gahtmod/item/ModItems.java
+++ src/main/java/dev/lumelore/gahtmod/item/ModItems.java
@@ -12,14 +12,14 @@ public abstract class ModItems {
     // Creative Tab Icon - Not Meant To Be Obtainable
     public static final Item GAHT_ICON = registerItem("gaht_icon", new Item(new FabricItemSettings().maxCount(0)));
 
-    public static final Item ESTROGEN_PILL = registerItem("estrogen_pill", new Item(new FabricItemSettings()));
+    public static final Item ESTROGEN_PILL = registerItem("estrogen_pill", new Item(new FabricItemSettings().food(ModFoodComponents.ESTROGEN_ITEM)));
     public static final Item TESTOSTERONE_BLOCKER = registerItem("testosterone_blocker", new Item(new FabricItemSettings()));
     public static final Item PROGESTERONE_CAPSULE = registerItem("progesterone_capsule", new Item(new FabricItemSettings()));
-    public static final Item TESTOSTERONE_GEL_CAPSULE = registerItem("testosterone_gel_capsule", new Item(new FabricItemSettings()));
+    public static final Item TESTOSTERONE_GEL_CAPSULE = registerItem("testosterone_gel_capsule", new Item(new FabricItemSettings().food(ModFoodComponents.TESTOSTERONE_ITEM)));
     public static final Item ESTROGEN_BLOCKER = registerItem("estrogen_blocker", new Item(new FabricItemSettings()));
 
-    public static final Item VIAL_OF_ESTROGEN = registerItem("vial_of_estrogen", new Item(new FabricItemSettings()));
-    public static final Item VIAL_OF_TESTOSTERONE = registerItem("vial_of_testosterone", new Item(new FabricItemSettings()));
+    public static final Item VIAL_OF_ESTROGEN = registerItem("vial_of_estrogen", new Item(new FabricItemSettings().food(ModFoodComponents.STRONG_ESTROGEN_ITEM)));
+    public static final Item VIAL_OF_TESTOSTERONE = registerItem("vial_of_testosterone", new Item(new FabricItemSettings().food(ModFoodComponents.STRONG_TESTOSTERONE_ITEM)));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(GAHTMod.MOD_ID, name), item);
