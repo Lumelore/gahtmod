5aa0e3a9d51a3f88ff5fc8ca0b061be5fac18fb4
Lumelore <itslumelore@gmail.com>
2024-04-28 21:29:08 -0500

    large juice gives 3 bottle back :)

---
+++ src/main/java/dev/lumelore/gahtmod/item/special/LargeEnbyJuiceItem.java
@@ -0,0 +1,66 @@
+package dev.lumelore.gahtmod.item.special;
+
+import net.minecraft.advancement.criterion.Criteria;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.stat.Stats;
+import net.minecraft.util.UseAction;
+import net.minecraft.world.World;
+
+public class LargeEnbyJuiceItem extends Item {
+
+    public LargeEnbyJuiceItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
+        super.finishUsing(stack, world, user);
+        // Add to item usage statistics
+        if (user instanceof ServerPlayerEntity) {
+            ServerPlayerEntity serverPlayerEntity = (ServerPlayerEntity)user;
+            Criteria.CONSUME_ITEM.trigger(serverPlayerEntity, stack);
+            serverPlayerEntity.incrementStat(Stats.USED.getOrCreateStat(this));
+        }
+        // Give the player a glass bottle when they are done drinking it
+        if (stack.isEmpty()) {
+            ItemStack tempStack = new ItemStack(Items.GLASS_BOTTLE);
+            tempStack.setCount(3);
+            return tempStack;
+        }
+        if (user instanceof PlayerEntity) {
+            PlayerEntity playerEntity = (PlayerEntity)user;
+            if (!playerEntity.getAbilities().creativeMode) {
+                ItemStack itemStack = new ItemStack(Items.GLASS_BOTTLE);
+                itemStack.setCount(3);
+                if (!playerEntity.getInventory().insertStack(itemStack)) {
+                    playerEntity.dropItem(itemStack, false);
+                }
+            }
+        }
+        return stack;
+    }
+
+    @Override
+    public UseAction getUseAction(ItemStack stack) {
+        return UseAction.DRINK;
+    }
+
+    @Override
+    public SoundEvent getDrinkSound() {
+        return SoundEvents.ITEM_HONEY_BOTTLE_DRINK;
+    }
+
+    @Override
+    public SoundEvent getEatSound() {
+        return SoundEvents.ITEM_HONEY_BOTTLE_DRINK;
+    }
+
+
+}
