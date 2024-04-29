640118ecca115130ff5500b17bf544de66ae3778
Lumelore <itslumelore@gmail.com>
2024-04-28 23:00:05 -0500

    have to make this to put tooltip on item i think

---
+++ src/main/java/dev/lumelore/gahtmod/item/special/ProgesteroneCapsuleItem.java
@@ -0,0 +1,40 @@
+package dev.lumelore.gahtmod.item.special;
+
+import dev.lumelore.gahtmod.effect.ModEffects;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffectUtil;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class ProgesteroneCapsuleItem extends Item {
+
+    public ProgesteroneCapsuleItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        tooltip.add(Text.translatable(ModEffects.GIRL_POWER.getTranslationKey()).formatted(ModEffects.GIRL_POWER.getCategory().getFormatting())
+                .append(" (")
+                .append(StatusEffectUtil.getDurationText(new StatusEffectInstance(ModEffects.GIRL_POWER, 9600), 1f, world == null ? 20.0f : world.getTickManager().getTickRate()))
+                .append(")"));
+        tooltip.add(Text.translatable(StatusEffects.REGENERATION.getTranslationKey()).formatted(StatusEffects.REGENERATION.getCategory().getFormatting())
+                .append(" (")
+                .append(StatusEffectUtil.getDurationText(new StatusEffectInstance(StatusEffects.REGENERATION, 1200), 1f, world == null ? 20.0f : world.getTickManager().getTickRate()))
+                .append(")"));
+        tooltip.add(Text.translatable("tooltip.gahtmod.blocker_item").formatted(Formatting.DARK_PURPLE));
+        tooltip.add(Text.translatable(ModEffects.BOY_POWER.getTranslationKey()).formatted(ModEffects.BOY_POWER.getCategory().getFormatting()));
+        tooltip.add(Text.translatable(StatusEffects.SLOWNESS.getTranslationKey()).formatted(StatusEffects.SLOWNESS.getCategory().getFormatting()));
+        tooltip.add(Text.translatable(StatusEffects.WEAKNESS.getTranslationKey()).formatted(StatusEffects.WEAKNESS.getCategory().getFormatting()));
+        tooltip.add(Text.translatable(StatusEffects.WITHER.getTranslationKey()).formatted(StatusEffects.WITHER.getCategory().getFormatting()));
+        super.appendTooltip(stack, world, tooltip, context);
+    }
+}
