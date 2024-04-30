d80892fee605661a2de311d28e2c8f65cdc0319b
Lumelore <itslumelore@gmail.com>
2024-04-30 15:59:35 -0500

    mixin stuff

--- src/main/java/dev/lumelore/mixin/PlayerEntityAccessor.java
+++ src/main/java/dev/lumelore/mixin/PlayerEntityAccessor.java
@@ -4,6 +4,7 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -15,5 +16,6 @@ public interface PlayerEntityAccessor {
 
 	@Accessor
 	int getJumpingCooldown();
+
 }
 
