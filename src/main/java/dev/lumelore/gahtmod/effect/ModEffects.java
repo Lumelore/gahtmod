c85f04d30075d6aafd5b409d1a5c26100d5d1075
Lumelore <itslumelore@gmail.com>
2024-04-30 16:01:12 -0500

    added attributes to effects to reduce fall damage

--- src/main/java/dev/lumelore/gahtmod/effect/ModEffects.java
+++ src/main/java/dev/lumelore/gahtmod/effect/ModEffects.java
@@ -1,6 +1,8 @@
 package dev.lumelore.gahtmod.effect;
 
 import dev.lumelore.gahtmod.GAHTMod;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -12,8 +14,9 @@ import static dev.lumelore.gahtmod.GAHTMod.MOD_ID;
 public class ModEffects {
 
     // Registers for effects
-    public static final RegistryEntry<StatusEffect> GIRL_POWER = Registry.registerReference(Registries.STATUS_EFFECT, new Identifier(MOD_ID, "girl_power"), new GirlPowerEffect());
-    public static final RegistryEntry<StatusEffect> BOY_POWER = Registry.registerReference(Registries.STATUS_EFFECT, new Identifier(MOD_ID, "boy_power"), new BoyPowerEffect());
+    // Girl + Boy power effect have same uuid for attribute modifier because I don't want them to stack
+    public static final RegistryEntry<StatusEffect> GIRL_POWER = Registry.registerReference(Registries.STATUS_EFFECT, new Identifier(MOD_ID, "girl_power"), new GirlPowerEffect().addAttributeModifier(EntityAttributes.GENERIC_SAFE_FALL_DISTANCE, "822AE5A6-8C1E-ADE7-Df79-CB606A1B66A1", 2.0, EntityAttributeModifier.Operation.ADD_VALUE));
+    public static final RegistryEntry<StatusEffect> BOY_POWER = Registry.registerReference(Registries.STATUS_EFFECT, new Identifier(MOD_ID, "boy_power"), new BoyPowerEffect().addAttributeModifier(EntityAttributes.GENERIC_SAFE_FALL_DISTANCE,    "822AE5A6-8C1E-ADE7-Df79-CB606A1B66A1", 2.0, EntityAttributeModifier.Operation.ADD_VALUE));
     public static final RegistryEntry<StatusEffect> ENBY_POWER = Registry.registerReference(Registries.STATUS_EFFECT, new Identifier(MOD_ID, "enby_power"), new EnbyPowerEffect());
 
     public static void registerModEffects() {
